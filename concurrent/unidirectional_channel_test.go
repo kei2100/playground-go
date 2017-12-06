a10e94e9f63355bbdd7115f395a2ae37a6f85b52
Kei Arima <kei.arima@freee.co.jp>
2017-12-06 12:33:56 +0900

    Add concurrent

---
+++ concurrent/unidirectional_channel_test.go
@@ -0,0 +1,31 @@
+package concurrent
+
+import (
+	"sync"
+	"fmt"
+)
+
+func ExampleUnidirectionalChannel() {
+	ch := make(chan int)
+	wg := sync.WaitGroup{}
+
+	sender := func(c chan<- int) {
+		c <- 1
+		wg.Done()
+	}
+	receiver := func(c <-chan int) {
+		v := <-c
+		fmt.Println(v)
+		wg.Done()
+
+		// Output:
+		// 1
+	}
+
+	wg.Add(1)
+	go sender(ch)
+	wg.Add(1)
+	go receiver(ch)
+
+	wg.Wait()
+}
