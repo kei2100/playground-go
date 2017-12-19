acfa13f348cbfcf3b3204521acf2d948f545e9f2
Kei Arima <kei.arima@freee.co.jp>
2017-12-19 19:49:20 +0900

    add

--- regexp/copy_bench_test.go
+++ regexp/copy_bench_test.go
@@ -6,24 +6,40 @@ import (
 )
 
 func BenchmarkRegexpShared(b *testing.B) {
-	x := []byte("this is a long line that contains foo bar baz")
+	x := "this is a long line that contains foo bar baz"
 	re := regexp.MustCompile("foo (ba+r)? baz")
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			re.Match(x)
+			re.FindStringSubmatchIndex(x)
 		}
 	})
 }
 
 func BenchmarkRegexpCopied(b *testing.B) {
-	x := []byte("this is a long line that contains foo bar baz")
+	x := "this is a long line that contains foo bar baz"
 	re := regexp.MustCompile("foo (ba+r)? baz")
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		re := re.Copy()
 		for pb.Next() {
-			re.Match(x)
+			re.FindStringSubmatchIndex(x)
 		}
 	})
+}
+
+func BenchmarkRegexpFindStringSubmatchIndex(b *testing.B) {
+	r := regexp.MustCompile(`\A(\d{1,2}):(\d{1,2})(?::(\d{1,2}))?`)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r.FindStringSubmatchIndex("12:11:10")
+	}
+}
+
+func BenchmarkRegexpFindStringSubmatch(b *testing.B) {
+	r := regexp.MustCompile(`\A(\d{1,2}):(\d{1,2})(?::(\d{1,2}))?`)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r.FindStringSubmatch("12:11:10")
+	}
 }
