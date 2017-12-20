0ae19300cfb4d59a681eeb111253791a7fdb94e3
Kei Arima <kei.arima@freee.co.jp>
2017-12-20 10:54:37 +0900

    w

--- concurrency/pool_test.go
+++ concurrency/pool_test.go
@@ -1,6 +1,39 @@
 package concurrency
 
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
 // sync.Poolは一時的なオブジェクトのPool
 // groutineセーフにオブジェクトを共有するPoolからGetしたり、Putしたりすることができる。
 // 内部は弱参照になっており、sync.Pool内でしか参照されていないオブジェクトは、
 // 予告なしに解放される可能性がある。
+
+type mark struct {
+	used bool
+}
+
+func TestPool(t *testing.T) {
+	p := &sync.Pool{
+		New: func() interface{} {
+			return mark{}
+		},
+	}
+	wg := new(sync.WaitGroup)
+	const n = 1000
+	wg.Add(n)
+
+	for i := 0; i < n; i++ {
+		go func(num int) {
+			defer wg.Done()
+			b := p.Get().(mark)
+			fmt.Printf("num:%v used:%v\n", num, b.used)
+			b.used = true
+			p.Put(b)
+		}(i)
+	}
+
+	wg.Wait()
+}
