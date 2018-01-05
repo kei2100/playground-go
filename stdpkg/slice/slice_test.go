fe6d7a76ff38308f9684615f82defd6d6ba68f5b
Kei Arima <kei.arima@freee.co.jp>
2018-01-05 23:47:16 +0900

    add

---
+++ stdpkg/slice/slice_test.go
@@ -0,0 +1,19 @@
+package slice
+
+import "fmt"
+
+func ExampleCap() {
+	s1 := make([]struct{}, 0)
+	s2 := append(s1, struct{}{})
+	s3 := make([]struct{}, 0, 10)
+
+	// capで内部の配列のサイズを検証する
+
+	// Output:
+	// 0
+	// 1
+	// 10
+	fmt.Println(cap(s1))
+	fmt.Println(cap(s2))
+	fmt.Println(cap(s3))
+}
