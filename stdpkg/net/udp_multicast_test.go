25ba8b698bd26aa251f17add68bc1c9d673d2515
Kei Arima <kei.arima@freee.co.jp>
2018-01-09 00:08:13 +0900

    mod

--- stdpkg/net/udp_multicast_test.go
+++ stdpkg/net/udp_multicast_test.go
@@ -1,10 +1,11 @@
 package net
 
 import (
+	"context"
 	"encoding/binary"
 	"fmt"
-	"log"
 	"net"
+	"sync/atomic"
 	"testing"
 	"time"
 )
@@ -42,46 +43,107 @@ func (s *multicastUnixTimeServer) serve() error {
 }
 
 func (s *multicastUnixTimeServer) stop() {
-	s.stopSig <- struct{}{}
+	close(s.stopSig)
 	<-s.done
+}
+
+type multicastClient struct {
+	stopSig chan struct{}
+	done    chan struct{}
+}
+
+func (c *multicastClient) listen() (<-chan []byte, error) {
+	addr, err := net.ResolveUDPAddr("udp4", "224.0.0.1:9999")
+	if err != nil {
+		return nil, fmt.Errorf("net: failed to resolve addr: %v", err)
+	}
+
+	conn, err := net.ListenMulticastUDP("udp4", nil, addr)
+	if err != nil {
+		return nil, fmt.Errorf("net: failed to listen: %v", err)
+	}
+
+	data := make(chan []byte)
+	c.stopSig, c.done = make(chan struct{}), make(chan struct{})
+
+	go func() {
+		defer func() {
+			conn.Close()
+			close(data)
+			close(c.done)
+		}()
+
+		for {
+			select {
+			case <-c.stopSig:
+				return
+			default:
+				b := make([]byte, 8)
+				l, _, err := conn.ReadFromUDP(b)
+				if err != nil {
+					fmt.Printf("net: faield to read from the connection: %v", err)
+					return
+				}
+				data <- b[:l]
+			}
+		}
+	}()
+
+	return data, nil
+}
+
+func (c *multicastClient) stop() {
+	close(c.stopSig)
+	<-c.done
 }
 
 func TestUDPMulticast(t *testing.T) {
 	srv := &multicastUnixTimeServer{}
 	go func() {
-		log.Println(srv.serve())
+		fmt.Println(srv.serve())
 	}()
 	defer srv.stop()
 
-	addr, err := net.ResolveUDPAddr("udp4", "224.0.0.1:9999")
+	c1, c2 := &multicastClient{}, &multicastClient{}
+
+	d1, err := c1.listen()
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer c1.stop()
 
-	conn, err := net.ListenMulticastUDP("udp4", nil, addr)
+	d2, err := c2.listen()
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer conn.Close()
+	defer c2.stop()
 
-	cnt := 0
+	var cnt int32 = 0
+	ctx, can := context.WithTimeout(context.Background(), 5*time.Second)
+	defer can()
+
+	go func() {
+		for d := range d1 {
+			atomic.AddInt32(&cnt, 1)
+			fmt.Printf("d1 %v\n", binary.LittleEndian.Uint64(d))
+		}
+	}()
+
+	go func() {
+		for d := range d2 {
+			atomic.AddInt32(&cnt, 1)
+			fmt.Printf("d2 %v\n", binary.LittleEndian.Uint64(d))
+		}
+	}()
 
 	for {
 		select {
-		case <-time.After(5 * time.Second):
+		case <-ctx.Done():
 			t.Fatal("timeout exceeded")
 		default:
-			if cnt > 2 {
+			if cnt > 5 {
 				return
 			}
-
-			b := make([]byte, 8)
-			l, _, err := conn.ReadFrom(b)
-			if err != nil {
-				t.Fatal(err)
-			}
-			fmt.Println(binary.LittleEndian.Uint64(b[:l]))
-			cnt++
 		}
 	}
 }
