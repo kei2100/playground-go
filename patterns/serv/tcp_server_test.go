eaad49103b98d23d1b66c9d59f4df5cddbb41661
Kei Arima <kei.arima@freee.co.jp>
2018-01-12 10:41:08 +0900

    addtest

--- patterns/serv/tcp_server_test.go
+++ patterns/serv/tcp_server_test.go
@@ -2,6 +2,7 @@ package serv
 
 import (
 	"bufio"
+	"errors"
 	"io/ioutil"
 	"net"
 	"testing"
@@ -9,6 +10,8 @@ import (
 )
 
 func TestTCPServer_ServeClose(t *testing.T) {
+	t.Parallel()
+
 	s := &TCPServer{
 		handler: func(conn net.Conn) {
 			defer conn.Close()
@@ -64,4 +67,93 @@ func TestTCPServer_ServeClose(t *testing.T) {
 	case <-time.After(3 * time.Second):
 		t.Errorf("timeout exceeded while waiting for serv Close")
 	}
+}
+
+// implements net.Listener. Accept() always return err
+type acceptErrorListener struct {
+	raise error
+}
+
+func (l *acceptErrorListener) Accept() (net.Conn, error) {
+	return nil, l.raise
+}
+
+func (l *acceptErrorListener) Close() error {
+	return nil
+}
+
+func (l *acceptErrorListener) Addr() net.Addr {
+	a, _ := net.ResolveTCPAddr("tcp", "192.0.2.0:0")
+	return a
+}
+
+// represents a temporary net.Error
+type temporaryError struct{}
+
+func (e *temporaryError) Error() string {
+	return "temporary error"
+}
+
+func (e *temporaryError) Timeout() bool {
+	return true
+}
+
+func (e *temporaryError) Temporary() bool {
+	return true
+}
+
+func TestTCPServer_HandleAcceptError(t *testing.T) {
+	t.Parallel()
+
+	t.Run("temporary error", func(t *testing.T) {
+		ln := &acceptErrorListener{raise: new(temporaryError)}
+		s := new(TCPServer)
+
+		closed := make(chan struct{})
+		go func() {
+			err := s.Serve(ln)
+			if g, w := err, ErrServerClosed; g != w {
+				t.Errorf("Serve returns got %v, want %v", g, w)
+			}
+			close(closed)
+		}()
+
+		select {
+		case <-closed:
+			t.Fatal("server got closed, want sleep and call next Accept()")
+		case <-time.After(50 * time.Millisecond):
+			// ok
+		}
+
+		s.Close()
+
+		select {
+		case <-closed:
+			// test ok
+		case <-time.After(3 * time.Second):
+			t.Errorf("timeout exceeded while waiting for serv Close")
+		}
+	})
+
+	t.Run("other error", func(t *testing.T) {
+		want := errors.New("other error")
+		ln := &acceptErrorListener{raise: want}
+		s := new(TCPServer)
+
+		closed := make(chan struct{})
+		go func() {
+			got := s.Serve(ln)
+			if g, w := got.Error(), want.Error(); g != w {
+				t.Errorf("Serve returns got %v, want %v", g, w)
+			}
+			close(closed)
+		}()
+
+		select {
+		case <-closed:
+			// test ok
+		case <-time.After(3 * time.Second):
+			t.Errorf("timeout exceeded while waiting for serv Close")
+		}
+	})
 }
