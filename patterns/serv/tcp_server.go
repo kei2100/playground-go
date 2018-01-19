64a52b3c4e37d326fe47674b61416ed0b1f32b6b
Kei Arima <kei.arima@freee.co.jp>
2018-01-19 10:38:52 +0900

    mod

--- patterns/serv/tcp_server.go
+++ patterns/serv/tcp_server.go
@@ -35,27 +35,72 @@ func (st *servState) setClosed() {
 	*st = stateClosed
 }
 
+type TCPConnOptions struct {
+	KeepAlive       bool
+	KeepAlivePeriod time.Duration
+	ReadTimeout     time.Duration
+	WriteTimeout    time.Duration
+}
+
+func defaultTCPConnOptions() TCPConnOptions {
+	return TCPConnOptions{
+		KeepAlive:       true,
+		KeepAlivePeriod: 1 * time.Minute,
+		ReadTimeout:     3 * time.Minute,
+	}
+}
+
+type TCPServerOptions func(*TCPServer)
+
+func WithKeepAlive(keepalive bool) TCPServerOptions {
+	return func(s *TCPServer) {
+		s.connOpts.KeepAlive = keepalive
+	}
+}
+
+func WithKeepAlivePeriod(p time.Duration) TCPServerOptions {
+	return func(s *TCPServer) {
+		s.connOpts.KeepAlivePeriod = p
+	}
+}
+
+func WithReadTimeout(t time.Duration) TCPServerOptions {
+	return func(s *TCPServer) {
+		s.connOpts.ReadTimeout = t
+	}
+}
+
+func WithWriteTimeout(t time.Duration) TCPServerOptions {
+	return func(s *TCPServer) {
+		s.connOpts.WriteTimeout = t
+	}
+}
+
+func WithConnOptions(o TCPConnOptions) TCPServerOptions {
+	return func(s *TCPServer) {
+		s.connOpts = o
+	}
+}
+
 type TCPHandleFunc func(net.Conn)
 
 // TODO configure timeout
 // TODO shutdown
+// TODO error msg
 
 // base on http stdpkg
 type TCPServer struct {
 	mu sync.Mutex
+
 	servState
 	ln net.Listener
+
+	connOpts TCPConnOptions
 }
 
-func (s *TCPServer) Serve(ln net.Listener, handler TCPHandleFunc) error {
-	if err := s.withLockDo(func() error {
-		if s.IsListening() {
-			return fmt.Errorf("serv: already listening")
-		}
-		s.ln = ln
-		s.setListening()
-		return nil
-	}); err != nil {
+func (s *TCPServer) Serve(ln net.Listener, handler TCPHandleFunc, opts ...TCPServerOptions) error {
+	s.setOptions(opts...)
+	if err := s.setListener(ln); err != nil {
 		return err
 	}
 
@@ -83,11 +128,41 @@ func (s *TCPServer) Serve(ln net.Listener, handler TCPHandleFunc) error {
 			return err
 		}
 		tempDelay = 0
+
 		go handler(conn)
 	}
 }
 
 func (s *TCPServer) Close() error {
+	return s.closeListener()
+}
+
+func (s *TCPServer) Shutdown(ctx context.Context) error {
+	return nil
+}
+
+func (s *TCPServer) setOptions(opts ...TCPServerOptions) {
+	s.withLockDo(func() error {
+		s.connOpts = defaultTCPConnOptions()
+		for _, o := range opts {
+			o(s)
+		}
+		return nil
+	})
+}
+
+func (s *TCPServer) setListener(ln net.Listener) error {
+	return s.withLockDo(func() error {
+		if s.IsListening() {
+			return fmt.Errorf("serv: already listening")
+		}
+		s.ln = ln
+		s.setListening()
+		return nil
+	})
+}
+
+func (s *TCPServer) closeListener() error {
 	return s.withLockDo(func() error {
 		if s.IsClosed() {
 			return nil
@@ -100,13 +175,14 @@ func (s *TCPServer) Close() error {
 	})
 }
 
-func (s *TCPServer) Shutdown(ctx context.Context) error {
-	return nil
-}
-
 func (s *TCPServer) withLockDo(f func() error) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
 	return f()
+}
+
+type tcpServerConn struct {
+	net.Conn
+	s *TCPServer
 }
