a34f1c365a52e584c93832df356d70ed1c49fc66
Kei Arima <kei.arima@freee.co.jp>
2018-02-05 17:22:33 +0900

    add

---
+++ 3rdpkg/gops/example_gops.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"net"
+	"net/http"
+	"strconv"
+
+	"github.com/google/gops/agent"
+)
+
+func main() {
+	if err := agent.Listen(&agent.Options{}); err != nil {
+		log.Fatal(err)
+	}
+	ln, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("http server listening on %v", ln.Addr().String())
+	http.Serve(ln, serveMux())
+}
+
+func serveMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/delay", delay)
+	return mux
+}
+
+func index(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(200)
+}
+
+func delay(w http.ResponseWriter, r *http.Request) {
+	sec := r.URL.Query()["sec"]
+	var s string
+	if len(sec) > 0 {
+		s = sec[0]
+	}
+	d, err := strconv.Atoi(s)
+	if err != nil {
+		d = 0
+	}
+	if d > 300 {
+		d = 300
+	}
+	time.Sleep(time.Duration(d) * time.Second)
+	w.WriteHeader(200)
+}
