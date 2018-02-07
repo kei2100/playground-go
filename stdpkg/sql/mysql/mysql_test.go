68db82b03d59a361c475de4cd566d9bfe220d653
Kei Arima <kei.arima@freee.co.jp>
2018-02-07 19:11:48 +0900

    add

--- stdpkg/sql/mysql/mysql_test.go
+++ stdpkg/sql/mysql/mysql_test.go
@@ -7,18 +7,24 @@ import (
 	"testing"
 	"time"
 
+	"context"
+	"sync"
+
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/kei2100/playground-go/util/wait"
+)
+
+// prepare
+// docker run -e MYSQL_ROOT_PASSWORD=pass -p 13306:3306 -d mysql:5.7
+var (
+	idPass   = "root:pass"
+	hostPort = net.JoinHostPort("0.0.0.0", "13306")
 )
 
 func TestConnMaxLifetime(t *testing.T) {
 	t.Skip("skip for automated test")
 
-	// prepare
-	// docker run -e MYSQL_ROOT_PASSWORD=pass -p 13306:3306 -d mysql:5.7
-
-	ip := "root:pass"
-	hp := net.JoinHostPort("0.0.0.0", "13306")
-	db, err := sql.Open("mysql", fmt.Sprintf("%s@tcp(%s)/", ip, hp))
+	db, err := sql.Open("mysql", fmt.Sprintf("%s@tcp(%s)/", idPass, hostPort))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -38,4 +44,41 @@ func TestConnMaxLifetime(t *testing.T) {
 	if _, err := db.Query("SELECT 1"); err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestUseContext(t *testing.T) {
+	t.Skip("skip for automated test")
+
+	db, err := sql.Open("mysql", fmt.Sprintf("%s@tcp(%s)/", idPass, hostPort))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	db.SetMaxOpenConns(1)
+	ctx, can := context.WithTimeout(context.Background(), 1*time.Second)
+	defer can()
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		if _, err := db.Query("SELECT sleep(3)"); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	time.Sleep(1 * time.Millisecond)
+
+	go func() {
+		defer wg.Done()
+		_, err := db.QueryContext(ctx, "SELECT 1")
+		if err == nil {
+			t.Errorf("go no error, want an error")
+		}
+		fmt.Printf("%T: %s", err, err)
+	}()
+
+	wait.WGroup(&wg, 10*time.Second)
 }
