01e93252aa4f9f627d0882fb95c9d24dcd1ca8c9
kei2100 <kei.arima.2100@gmail.com>
2018-02-13 00:19:43 +0900

    enable to test

--- stdpkg/sql/mysql/mysql_test.go
+++ stdpkg/sql/mysql/mysql_test.go
@@ -15,14 +15,14 @@ import (
 )
 
 // prepare
-// docker run -e MYSQL_ROOT_PASSWORD=pass -p 13306:3306 -d mysql:5.7
+// docker run -e MYSQL_ROOT_PASSWORD=pass -p 3306:3306 -d mysql:5.7
 var (
-	idPass   = "root:pass"
-	hostPort = net.JoinHostPort("0.0.0.0", "13306")
+	idPass   = "root:root"
+	hostPort = net.JoinHostPort("0.0.0.0", "3306")
 )
 
 func TestConnMaxLifetime(t *testing.T) {
-	t.Skip("skip for automated test")
+	t.Parallel()
 
 	db, err := sql.Open("mysql", fmt.Sprintf("%s@tcp(%s)/", idPass, hostPort))
 	if err != nil {
@@ -41,13 +41,17 @@ func TestConnMaxLifetime(t *testing.T) {
 	fmt.Println("sleep...")
 	time.Sleep(2 * time.Second)
 
+	// wait_timeout指定により既存の接続がタイムアウトしたときは、再作成してリトライする挙動をする。
+	// [mysql] 2018/02/13 00:12:31 packets.go:33: unexpected EOFのようなstderr出力がある。
+	//
+	// db.SetConnMaxLifetime(1) にした場合はクライアント側でも古いコネクションを捨てるので、stderr出力はない。
 	if _, err := db.Query("SELECT 1"); err != nil {
 		t.Fatal(err)
 	}
 }
 
 func TestUseContext(t *testing.T) {
-	t.Skip("skip for automated test")
+	t.Parallel()
 
 	db, err := sql.Open("mysql", fmt.Sprintf("%s@tcp(%s)/", idPass, hostPort))
 	if err != nil {
