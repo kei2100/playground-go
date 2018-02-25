0705a24b4682d15028ace17373b644ab4177bdfe
kei2100 <kei.arima.2100@gmail.com>
2018-02-25 22:43:06 +0900

    wip

--- util/context/timestamp_context.go
+++ util/context/timestamp_context.go
@@ -6,19 +6,28 @@ import (
 	"time"
 )
 
+// TODO test
+
 // TimestampContext is the context in which time stamps can be recorded
 type TimestampContext interface {
 	context.Context
-	// Timestamp records the timestamp to this context with the given description
-	Timestamp(description string)
+	// DoTimestamp records the timestamp to this context with the given description
+	DoTimestamp(description string)
+	// ListTimestamps lists timestamps in this context
+	ListTimestamps() []Timestamp
+}
+
+// represents a Timestamp
+type Timestamp struct {
+	Description string
+	Time        time.Time
 }
 
 // DoTimestamp records the timestamp to the context with the given description.
 // If the context does not implement TimestampContext, nothing happens.
-// TODO 入れ替え
 func DoTimestamp(ctx context.Context, description string) {
 	if ctx, ok := ctx.(TimestampContext); ok {
-		ctx.Timestamp(description)
+		ctx.DoTimestamp(description)
 	}
 }
 
@@ -44,8 +53,22 @@ type timestampContextOptions struct {
 // TimestampContextOptionsFunc is a type of functional options for the TimeStampContext
 type TimestampContextOptionsFunc func(*timestampContextOptions)
 
+// TimestampMax configures timestampContext.maxStamps
+func TimestampMax(max int) TimestampContextOptionsFunc {
+	return func(o *timestampContextOptions) {
+		o.maxStamps = max
+	}
+}
+
+// TimestampSince configures timestampContext.since
+func TimestampSince(since time.Time) TimestampContextOptionsFunc {
+	return func(o *timestampContextOptions) {
+		o.since = since
+	}
+}
+
 // WithTimestamp creates the TimestampContext
-func WithTimestamp(parent context.Context, opts ...TimestampContextOptionsFunc) {
+func WithTimestamp(parent context.Context, opts ...TimestampContextOptionsFunc) TimestampContext {
 	conf := timestampContextOptions{
 		maxStamps: 30,
 		since:     time.Now(),
@@ -53,14 +76,37 @@ func WithTimestamp(parent context.Context, opts ...TimestampContextOptionsFunc) 
 	for _, o := range opts {
 		o(&conf)
 	}
-
+	c := timestampContext{
+		Context:   parent,
+		createdAt: conf.since,
+		maxStamps: conf.maxStamps,
+		stamps:    make([]elapsed, 0, conf.maxStamps),
+	}
+	return &c
 }
 
-func (c *timestampContext) Timestamp(description string) {
+func (c *timestampContext) DoTimestamp(description string) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	c.stamps = append(c.stamps, elapsed{
-		description: description,
-		elapsed:     time.Now().Sub(c.createdAt),
-	})
+
+	if len(c.stamps) < c.maxStamps {
+		c.stamps = append(c.stamps, elapsed{
+			description: description,
+			elapsed:     time.Now().Sub(c.createdAt),
+		})
+	}
+}
+
+func (c *timestampContext) ListTimestamps() []Timestamp {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	ret := make([]Timestamp, len(c.stamps))
+	for i, s := range c.stamps {
+		ret[i] = Timestamp{
+			Description: s.description,
+			Time:        c.createdAt.Add(s.elapsed),
+		}
+	}
+	return ret
 }
