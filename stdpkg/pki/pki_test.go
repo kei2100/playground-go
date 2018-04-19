456f186f9665d0129524152c6274022a5ba0ab6a
kei2100 <kei.arima.2100@gmail.com>
2018-04-19 15:12:38 +0900

    Mod pki

--- stdpkg/pki/pki_test.go
+++ stdpkg/pki/pki_test.go
@@ -10,7 +10,15 @@ import (
 	"testing"
 	"time"
 
+	"io/ioutil"
+
+	// 現状、標準パッケージだとpkcs12.Encodeが存在しない
+	"github.com/hashicorp/packer/builder/azure/pkcs12"
+
+	"encoding/base64"
+
 	"github.com/kei2100/playground-go/util/encoding/pem"
+	"path/filepath"
 )
 
 func TestSelfSignAsCA(t *testing.T) {
@@ -23,12 +31,6 @@ func TestSelfSignAsCA(t *testing.T) {
 
 func testSelfSignAsCA(t *testing.T) (*x509.Certificate, *rsa.PrivateKey) {
 	t.Helper()
-
-	priv, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		t.Fatal(err)
-	}
-	pub := priv.Public()
 
 	var sn int64 = 1
 	var subjKeyID = []byte{1, 2, 3, 4}
@@ -51,10 +53,14 @@ func testSelfSignAsCA(t *testing.T) (*x509.Certificate, *rsa.PrivateKey) {
 		SubjectKeyId:          subjKeyID,
 		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
 		BasicConstraintsValid: true,
-		IsCA:                  true,
+		IsCA: true,
 	}
 
-	certDER, err := x509.CreateCertificate(rand.Reader, &tmpl, &tmpl, pub, priv)
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		t.Fatal(err)
+	}
+	certDER, err := x509.CreateCertificate(rand.Reader, &tmpl, &tmpl, priv.Public(), priv)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -71,11 +77,7 @@ func TestIssueCerts(t *testing.T) {
 	ca, cakey := testSelfSignAsCA(t)
 	ckID := ca.SubjectKeyId
 
-	srvPriv, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		t.Fatal(err)
-	}
-
+	// サーバー証明書
 	srvTmpl := x509.Certificate{
 		SerialNumber: big.NewInt(ca.SerialNumber.Int64() + 1),
 
@@ -97,20 +99,23 @@ func TestIssueCerts(t *testing.T) {
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
 		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageDataEncipherment,
 		BasicConstraintsValid: true,
-		IsCA:                  false,
+		IsCA: false,
 	}
 
+	srvPriv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		t.Fatal(err)
+	}
 	srvCertDER, err := x509.CreateCertificate(rand.Reader, &srvTmpl, ca, srvPriv.Public(), cakey)
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	srvCert, err := x509.ParseCertificate(srvCertDER)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-
+	// クライアント証明書
 	clientTmpl := x509.Certificate{
 		SerialNumber: big.NewInt(ca.SerialNumber.Int64() + 2),
 
@@ -132,16 +137,78 @@ func TestIssueCerts(t *testing.T) {
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
 		KeyUsage:              x509.KeyUsageDigitalSignature,
 		BasicConstraintsValid: true,
-		IsCA:                  false,
+		IsCA: false,
 	}
 
+	clientPriv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		t.Fatal(err)
+	}
+	clientCertDER, err := x509.CreateCertificate(rand.Reader, &clientTmpl, ca, clientPriv.Public(), cakey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	clientCert, err := x509.ParseCertificate(clientCertDER)
+	if err != nil {
+		t.Fatal(err)
+	}
+	clientPFX, err := pkcs12.Encode(clientCertDER, clientPriv, "password")
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	fmt.Println("######### CA CERT ###########")
-	fmt.Println(string(pem.EncodeCertificateToMemory(ca)))
+	// CRL
+	r := pkix.RevokedCertificate{
+		SerialNumber:   clientCert.SerialNumber,
+		RevocationTime: time.Now(),
+	}
 
-	fmt.Println("######### SERVCER CERT ###########")
-	fmt.Println(string(pem.EncodeCertificateToMemory(srvCert)))
+	crlDER, err := ca.CreateCRL(rand.Reader, cakey, []pkix.RevokedCertificate{r}, time.Now(), ca.NotAfter) // time.Now()=ThisUpdate, ca.NotAfter=NextUpdate
+	if err != nil {
+		t.Fatal(err)
+	}
+	crl, err := x509.ParseCRL(crlDER)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	fmt.Println("######### SERVCER KEY ###########")
-	fmt.Println(string(pem.EncodeRSAPrivateKeyToMemory(srvPriv)))
+	// 出力
+	{
+		fmt.Println("######### CA CERT ###########")
+		n, b := "cacert.pem", pem.EncodeCertificateToMemory(ca)
+		fmt.Println(string(b))
+		writeFile(n, b)
+	}
+	{
+		fmt.Println("######### SERVCER CERT ###########")
+		n, b := "servcert.pem", pem.EncodeCertificateToMemory(srvCert)
+		fmt.Println(string(b))
+		writeFile(n, b)
+	}
+	{
+		fmt.Println("######### SERVCER KEY ###########")
+		n, b := "servkey.pem", pem.EncodeRSAPrivateKeyToMemory(srvPriv)
+		fmt.Println(string(b))
+		writeFile(n, b)
+	}
+	{
+		fmt.Println("######### CLIENT PKCS#12/pfx file ###########")
+		n, b := "client.pfx", clientPFX
+		fmt.Println(base64.StdEncoding.EncodeToString(b))
+		writeFile(n, b)
+	}
+	{
+		fmt.Println("######### CRL ###########")
+		n, b := "crl.pem", pem.EncodeCRLToMemory(crl)
+		fmt.Println(string(b))
+		writeFile(n, b)
+	}
+}
+
+func writeFile(name string, b []byte) {
+	// FIXME 実際に書き込みたいときは false > true に
+	if false {
+		p := filepath.Join(".", name)
+		ioutil.WriteFile(p, b, 0644)
+	}
 }
