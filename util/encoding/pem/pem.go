60f278b0d6c7c52400207dc072e2a82098ad04b1
kei2100 <kei.arima.2100@gmail.com>
2018-07-04 11:33:52 +0900

    Add encoder function

--- util/encoding/pem/pem.go
+++ util/encoding/pem/pem.go
@@ -1,11 +1,13 @@
 package pem
 
 import (
+	"crypto/ecdsa"
 	"crypto/rsa"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/asn1"
 	gopem "encoding/pem"
+	"fmt"
 )
 
 // EncodeCertificateToMemory encodes x509 certificate to PEM
@@ -20,6 +22,23 @@ func EncodeRSAPrivateKeyToMemory(k *rsa.PrivateKey) []byte {
 	return gopem.EncodeToMemory(&gopem.Block{
 		Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(k),
 	})
+}
+
+// EncodePrivateKeyToMemory encodes private key to PEM
+func EncodePrivateKeyToMemory(key interface{}) ([]byte, error) {
+	switch k := key.(type) {
+	case *rsa.PrivateKey:
+		return EncodeRSAPrivateKeyToMemory(k), nil
+	case *ecdsa.PrivateKey:
+		kb, err := x509.MarshalECPrivateKey(k)
+		if err != nil {
+			return nil, fmt.Errorf("pem: failed to marshal ecdsa private key: %v", err)
+		}
+		pb := gopem.EncodeToMemory(&gopem.Block{Type: "EC PRIVATE KEY", Bytes: kb})
+		return pb, nil
+	default:
+		return nil, fmt.Errorf("pem: unknown private key type %T", key)
+	}
 }
 
 // EncodeCRLToMemory encodes CRL to PEM
