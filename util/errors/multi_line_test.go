191b5a4c264c13266dc8796566d082db5e63ba45
kei2100 <kei.arima.2100@gmail.com>
2018-07-12 23:25:57 +0900

    Add error utils

---
+++ util/errors/multi_line_test.go
@@ -0,0 +1,18 @@
+package errors
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMultiLine_Error(t *testing.T) {
+	e := NewMultiLine(fmt.Errorf("foo"))
+	e.Add(fmt.Errorf("bar"))
+
+	want := fmt.Sprintln("foo")
+	want += "bar"
+
+	if g, w := e.Error(), want; g != w {
+		t.Errorf(" got %v, want %v", g, w)
+	}
+}
