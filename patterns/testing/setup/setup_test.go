599caeeacea0155cf9a7fe7c43574c8f0d81c421
kei2100 <kei.arima.2100@gmail.com>
2018-09-02 21:05:03 +0900

    Fix setup teardown

--- patterns/testing/setup/setup_test.go
+++ patterns/testing/setup/setup_test.go
@@ -3,36 +3,31 @@ package setup
 import "testing"
 
 var var1 = "var1"
-var var2 = "var2"
 
-func WithMockVar1(t *testing.T, f func(*testing.T)) {
+func setup(t *testing.T) (teardown func(*testing.T)) {
+	t.Helper()
 	bk := var1
 	var1 = "mock1"
-	defer func() { var1 = bk }()
 
-	f(t)
-}
-
-func WithMockVar2(t *testing.T, f func(*testing.T)) {
-	bk := var2
-	var2 = "mock2"
-	defer func() { var2 = bk }()
-
-	f(t)
+	return func(t *testing.T) {
+		t.Helper()
+		var1 = bk
+	}
 }
 
 func TestVar(t *testing.T) {
-	WithMockVar1(t, func(t *testing.T) {
-		WithMockVar2(t, func(t *testing.T) {
-			if g, w := var1, "mock1"; g != w {
-				t.Errorf("var1 got %v, want %v", g, w)
-			}
+	t.Run("use setup", func(t *testing.T) {
+		teardown := setup(t)
+		defer teardown(t)
 
-			if g, w := var2, "mock2"; g != w {
-				t.Errorf("var2 got %v, want %v", g, w)
-			}
-		})
+		if g, w := var1, "mock1"; g != w {
+			t.Errorf("var1 got %v, want %v", g, w)
+		}
 	})
 
-	t.Error("to be improve")
+	t.Run("not use setup", func(t *testing.T) {
+		if g, w := var1, "var1"; g != w {
+			t.Errorf("var1 got %v, want %v", g, w)
+		}
+	})
 }
