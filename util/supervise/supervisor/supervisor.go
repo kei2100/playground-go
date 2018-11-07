7ca2eb8194c660ad96beb92a12e5fb7308dfd1b7
kei2100 <kei.arima.2100@gmail.com>
2018-11-07 16:46:08 +0900

    Fix

--- util/supervise/supervisor/supervisor.go
+++ util/supervise/supervisor/supervisor.go
@@ -10,7 +10,8 @@ import (
 	"syscall"
 )
 
-type supervisor struct {
+// Supervisor struct
+type Supervisor struct {
 	exec string
 	args []string
 
@@ -21,17 +22,17 @@ type supervisor struct {
 	stopMu sync.RWMutex
 }
 
-// NewSupervisor creates a supervisor
-func NewSupervisor(exec string, args ...string) *supervisor {
-	return &supervisor{
+// NewSupervisor creates a Supervisor
+func NewSupervisor(exec string, args ...string) *Supervisor {
+	return &Supervisor{
 		exec: exec,
 		args: args,
 	}
 }
 
-// Start supervisor
-func (sv *supervisor) Start() error {
-	log.Printf("supervisor: started. pid %d", os.Getpid())
+// Start Supervisor
+func (sv *Supervisor) Start() error {
+	log.Printf("Supervisor: started. pid %d", os.Getpid())
 	if err := sv.startWorker(); err != nil {
 		return err
 	}
@@ -39,45 +40,45 @@ func (sv *supervisor) Start() error {
 	for {
 		err := sv.waitWorker()
 		if err != nil {
-			log.Printf("supervisor: woker abnormally finished. %v", err)
+			log.Printf("Supervisor: woker abnormally finished. %v", err)
 		}
 		if sv.stopCalled() {
 			return nil
 		}
-		log.Println("supervisor: restarting the worker...")
+		log.Println("Supervisor: restarting the worker...")
 		if err := sv.startWorker(); err != nil {
 			return err
 		}
 	}
 }
 
-func (sv *supervisor) startWorker() error {
+func (sv *Supervisor) startWorker() error {
 	sv.workerMu.Lock()
 	defer sv.workerMu.Unlock()
 	c := exec.Command(sv.exec, sv.args...)
 	c.Stdout, c.Stderr = os.Stdout, os.Stderr
 	if err := c.Start(); err != nil {
-		return fmt.Errorf("supervisor: failed to start the worker: %v", err)
+		return fmt.Errorf("Supervisor: failed to start the worker: %v", err)
 	}
-	log.Printf("supervisor: worker started. pid %d", c.Process.Pid)
+	log.Printf("Supervisor: worker started. pid %d", c.Process.Pid)
 	sv.worker = c
 	return nil
 }
 
-func (sv *supervisor) waitWorker() error {
+func (sv *Supervisor) waitWorker() error {
 	sv.workerMu.RLock()
 	w := sv.worker
 	sv.workerMu.RUnlock()
 	return w.Wait()
 }
 
-func (sv *supervisor) signalWorker(sig os.Signal) error {
+func (sv *Supervisor) signalWorker(sig os.Signal) error {
 	sv.workerMu.RLock()
 	defer sv.workerMu.RUnlock()
 	return sv.worker.Process.Signal(sig)
 }
 
-func (sv *supervisor) handleSignal() {
+func (sv *Supervisor) handleSignal() {
 	ch := make(chan os.Signal)
 	signal.Notify(ch, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGHUP)
 	for sig := range ch {
@@ -86,21 +87,21 @@ func (sv *supervisor) handleSignal() {
 			continue
 		}
 		sv.stopCall()
-		log.Println("supervisor: stopping the supervisor")
+		log.Println("Supervisor: stopping the Supervisor")
 		if err := sv.signalWorker(sig); err != nil {
-			log.Printf("supervisor: failed to send sig %s to the worker: %v", sig, err)
+			log.Printf("Supervisor: failed to send sig %s to the worker: %v", sig, err)
 		}
 		return
 	}
 }
 
-func (sv *supervisor) stopCall() {
+func (sv *Supervisor) stopCall() {
 	sv.stopMu.Lock()
 	defer sv.stopMu.Unlock()
 	sv.stop = true
 }
 
-func (sv *supervisor) stopCalled() bool {
+func (sv *Supervisor) stopCalled() bool {
 	sv.stopMu.RLock()
 	defer sv.stopMu.RUnlock()
 	return sv.stop
