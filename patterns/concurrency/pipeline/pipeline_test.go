18b542c19cc82dce73aed79f0132acf5bef5dd8c
kei2100 <kei.arima.2100@gmail.com>
2019-01-15 22:37:36 +0900

    Fix

--- patterns/concurrency/pipeline/pipeline_test.go
+++ patterns/concurrency/pipeline/pipeline_test.go
@@ -1,112 +1,314 @@
 package pipeline
 
 import (
-	gocontext "context"
-	"fmt"
-	"log"
-	"strings"
+	"context"
+	"sync"
 	"testing"
-	"unicode"
-
-	"github.com/kei2100/playground-go/util/context"
-	"golang.org/x/text/width"
+	"time"
+	"unicode/utf8"
 )
 
-func TestPipeline(t *testing.T) {
-	ctx, can := context.Failure(gocontext.Background())
+func generator(ctx context.Context, values ...interface{}) <-chan interface{} {
+	ch := make(chan interface{})
+	go func() {
+		defer close(ch)
+		for _, v := range values {
+			select {
+			case <-ctx.Done():
+				return
+			case ch <- v:
+			}
+		}
+
+	}()
+	return ch
+}
+
+func assertRecv(t *testing.T, ch <-chan interface{}, want interface{}) {
+	t.Helper()
+	ctx, can := context.WithTimeout(context.Background(), time.Second)
+	defer can()
+	assertRecvContext(ctx, t, ch, want)
+}
+
+func assertRecvContext(ctx context.Context, t *testing.T, ch <-chan interface{}, want interface{}) {
+	t.Helper()
+	done := ctx.Done()
+	select {
+	case <-done:
+		t.Errorf("timeout while waiting for %v", want)
+		return
+	case got, ok := <-ch:
+		if !ok {
+			t.Errorf("ch closed. want %v", want)
+		} else if got != want {
+			t.Errorf("recv %v, want %v", got, want)
+		}
+	}
+}
+
+func assertRecvSeq(t *testing.T, ch <-chan interface{}, wants ...interface{}) {
+	t.Helper()
+	ctx, can := context.WithTimeout(context.Background(), time.Second)
+	defer can()
+	assertRecvSeqContext(ctx, t, ch, wants...)
+}
+
+func assertRecvSeqContext(ctx context.Context, t *testing.T, ch <-chan interface{}, wants ...interface{}) {
+	t.Helper()
+	done := ctx.Done()
+	for i, want := range wants {
+		select {
+		case <-done:
+			t.Errorf("timeout while waiting for %v", wants[i:])
+			return
+		case got, ok := <-ch:
+			if !ok {
+				t.Errorf("ch closed. want %v", want)
+			} else if got != want {
+				t.Errorf("idx %v recv %v, want %v", i, got, want)
+			}
+		}
+	}
+}
+
+func TestGenerator(t *testing.T) {
+	ctx, can := context.WithTimeout(context.Background(), time.Second)
+	defer can()
+	ch := generator(ctx, 1, 2, 3)
+	assertRecvSeq(t, ch, 1, 2, 3)
+}
+
+func repeat(ctx context.Context, values ...interface{}) <-chan interface{} {
+	ch := make(chan interface{})
+	go func() {
+		defer close(ch)
+		for {
+			for _, v := range values {
+				if ok := sendOrDone(ctx, ch, v); !ok {
+					return
+				}
+			}
+		}
+	}()
+	return ch
+}
+
+func take(ctx context.Context, stream <-chan interface{}, num int) <-chan interface{} {
+	ch := make(chan interface{})
+	go func() {
+		defer close(ch)
+		i := 0
+		for v := range recvOrDone(ctx, stream) {
+			if i >= num {
+				return
+			}
+			sendOrDone(ctx, ch, v)
+			i++
+		}
+	}()
+	return ch
+}
+
+func TestRepeatTake(t *testing.T) {
+	ctx, can := context.WithTimeout(context.Background(), time.Second)
 	defer can()
 
-	c := gen(ctx, "ｈｅｌｌｏ", "ｗｏｒｌｄ", "ｇｏｌａｎｇ")
-	//c := gen(ctx, "ｈｅｌｌｏ", "ｗｏｒｌｄ", "ｇｏｌａｎｇ", string(unicode.ReplacementChar))	// error pattern
-	rs := toUpper(ctx, toNarrow(ctx, c))
+	ch := take(ctx, repeat(ctx, 1, 2, 3), 4)
+	cnt := 0
+	for v := range ch {
+		cnt += v.(int)
+	}
+	if g, w := cnt, 7; g != w {
+		t.Errorf("cnt got %v, want %v", g, w)
+	}
 
-	var ss []string
+	ch = take(ctx, take(ctx, repeat(ctx, 1), 2), 4)
+	cnt = 0
+	for v := range ch {
+		cnt += v.(int)
+	}
+	if g, w := cnt, 2; g != w {
+		t.Errorf("cnt got %v, want %v", g, w)
+	}
+}
 
-loop:
-	for {
-		select {
-		case s, ok := <-rs:
-			if !ok {
-				break loop
+func sendOrDone(ctx context.Context, to chan<- interface{}, v interface{}) bool {
+	select {
+	case <-ctx.Done():
+		return false
+	case to <- v:
+		return true
+	}
+}
+
+func recvOrDone(ctx context.Context, from <-chan interface{}) <-chan interface{} {
+	ch := make(chan interface{})
+	go func() {
+		defer close(ch)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case v, ok := <-from:
+				if !ok {
+					return
+				}
+				select {
+				case <-ctx.Done():
+					return
+				case ch <- v:
+				}
 			}
-			ss = append(ss, s)
-		case <-ctx.Done():
-			fmt.Printf("pipeline failed: %v\n", ctx.Err())
-			return
+		}
+	}()
+	return ch
+}
+
+func wordCount(ctx context.Context, stringStream <-chan interface{}) <-chan interface{} {
+	ch := make(chan interface{})
+	go func() {
+		defer close(ch)
+		for v := range recvOrDone(ctx, stringStream) {
+			n := utf8.RuneCountInString(v.(string))
+			sendOrDone(ctx, ch, n)
+		}
+	}()
+	return ch
+}
+
+func merge(ctx context.Context, streams ...<-chan interface{}) <-chan interface{} {
+	wg := sync.WaitGroup{}
+	merged := make(chan interface{})
+
+	drain := func(ch <-chan interface{}) {
+		defer wg.Done()
+		for v := range recvOrDone(ctx, ch) {
+			sendOrDone(ctx, merged, v)
 		}
 	}
 
-	fmt.Println(strings.Join(ss, " "))
+	for _, s := range streams {
+		wg.Add(1)
+		go drain(s)
+	}
+	go func() {
+		wg.Wait()
+		close(merged)
+	}()
+
+	return merged
 }
 
-// generate pipeline
-func gen(ctx gocontext.Context, strs ...string) <-chan string {
-	out := make(chan string)
+func TestFanOutFanIn(t *testing.T) {
+	ctx, can := context.WithTimeout(context.Background(), time.Second)
+	defer can()
 
+	words := generator(ctx, "foo", "barb", "bazzz")
+	// fan out
+	ch1 := wordCount(ctx, words)
+	ch2 := wordCount(ctx, words)
+	ch3 := wordCount(ctx, words)
+
+	// fan in
+	sum := 0
+	for cnt := range merge(ctx, ch1, ch2, ch3) {
+		sum += cnt.(int)
+	}
+	if g, w := sum, 12; g != w {
+		t.Errorf("sum got %v, want %v", g, w)
+	}
+}
+
+func tee(ctx context.Context, stream <-chan interface{}) (_, _ chan interface{}) {
+	ch1, ch2 := make(chan interface{}), make(chan interface{})
 	go func() {
 		defer func() {
-			log.Println("gen: finished. close out channel")
-			close(out)
+			close(ch1)
+			close(ch2)
 		}()
 
-		for _, s := range strs {
-			select {
-			case out <- s:
-				continue
-			case <-ctx.Done():
-				return
+		for v := range recvOrDone(ctx, stream) {
+			var c1, c2 = ch1, ch2
+			for i := 0; i < 2; i++ {
+				select {
+				case <-ctx.Done():
+					return
+				case c1 <- v:
+					c1 = nil
+				case c2 <- v:
+					c2 = nil
+				}
 			}
 		}
 	}()
-
-	return out
+	return ch1, ch2
 }
 
-func toNarrow(ctx context.FailureContext, in <-chan string) <-chan string {
-	out := make(chan string)
+func TestTee(t *testing.T) {
+	ch := make(chan interface{})
+	ctx, can := context.WithTimeout(context.Background(), time.Second)
+	defer can()
 
 	go func() {
-		defer func() {
-			log.Println("toNarrow: finished. close out channel")
-			close(out)
-		}()
+		sendOrDone(ctx, ch, 1)
+		sendOrDone(ctx, ch, 2)
+		sendOrDone(ctx, ch, 3)
+	}()
 
-		for s := range in {
+	tc1, tc2 := tee(ctx, ch)
+	assertRecv(t, tc1, 1)
+	assertRecv(t, tc2, 1)
+	assertRecv(t, tc1, 2)
+	assertRecv(t, tc2, 2)
+	assertRecv(t, tc1, 3)
+	assertRecv(t, tc2, 3)
+}
+
+func bridge(ctx context.Context, chStream <-chan <-chan interface{}) <-chan interface{} {
+	out := make(chan interface{})
+	go func() {
+		defer close(out)
+		for {
+			var in <-chan interface{}
 			select {
-			case out <- width.Narrow.String(s):
-				continue
 			case <-ctx.Done():
 				return
+			case ch, ok := <-chStream:
+				if !ok {
+					return
+				}
+				in = ch
+			}
+			for v := range recvOrDone(ctx, in) {
+				sendOrDone(ctx, out, v)
 			}
 		}
 	}()
-
 	return out
 }
 
-func toUpper(ctx context.FailureContext, in <-chan string) <-chan string {
-	out := make(chan string)
+func TestBridge(t *testing.T) {
+	ctx, can := context.WithTimeout(context.Background(), time.Second)
+	defer can()
 
+	ch1 := take(ctx, repeat(ctx, 1), 1)
+	ch2 := take(ctx, repeat(ctx, 2), 2)
+	chStream := make(chan (<-chan interface{}))
 	go func() {
-		defer func() {
-			log.Println("toUpper: finished. close out channel")
-			close(out)
-		}()
-
-		for s := range in {
-			s := strings.ToUpper(s)
-			if strings.ContainsRune(s, unicode.ReplacementChar) {
-				ctx.Fail(fmt.Errorf("invalid code points contains: %v", s))
-				return
-			}
-			select {
-			case out <- s:
-				continue
-			case <-ctx.Done():
-				return
-			}
+		defer close(chStream)
+		select {
+		case <-ctx.Done():
+			return
+		case chStream <- ch1:
+		}
+		select {
+		case <-ctx.Done():
+			return
+		case chStream <- ch2:
 		}
 	}()
 
-	return out
+	out := bridge(ctx, chStream)
+	assertRecvSeqContext(ctx, t, out, 1, 2, 2)
 }
