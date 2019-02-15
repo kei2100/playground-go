41b8a2e50fb208c2e50548b38d01cac2b4a16d6c
kei2100 <kei.arima.2100@gmail.com>
2019-02-15 10:58:46 +0900

    ad

--- 3rdpkg/go-ipc/fifo/wintest/check/errcheck_win.go
+++ 3rdpkg/go-ipc/fifo/wintest/check/errcheck_win.go
@@ -13,7 +13,16 @@ import (
 
 // Errcheck func
 func Errcheck() {
-	wfifo, err := fifo.New("fifo", os.O_CREATE|os.O_WRONLY|os.O_TRUNC|fifo.O_NONBLOCK, 0666)
+	rfifo, err := fifo.New("rfifo", os.O_CREATE|os.O_RDONLY|os.O_TRUNC|fifo.O_NONBLOCK, 0666)
+	if err != nil {
+			log.Printf("rfifo errno error: %T", err)
+			log.Printf("rfifo errno error: %v", err)
+	} else {
+		log.Println("rfifo close")
+		rfifo.Close()
+	}
+
+	wfifo, err := fifo.New("wfifo", os.O_CREATE|os.O_WRONLY|os.O_TRUNC|fifo.O_NONBLOCK, 0666)
 	if err != nil {
 		// linux
 		if err, ok := errors.Cause(err).(*os.PathError); ok {
