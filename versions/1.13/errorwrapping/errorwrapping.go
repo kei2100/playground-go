67250bfa9c3ba59e4c85506fac3585551b3a1d5e
kei2100 <kei.arima.2100@gmail.com>
2019-08-16 09:07:32 +0900

    add

--- versions/1.13/errorwrapping/errorwrapping.go
+++
@@ -1,3 +0,0 @@
-package errorwrapping
-
-var TODO = 0
---
+++ versions/1.13/errorwrapping/errorwrapping_test.go
@@ -0,0 +1,33 @@
+package errorwrapping
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"testing"
+)
+
+type cause struct{}
+
+func (*cause) Error() string {
+	return "cause!"
+}
+
+func TestFmtErrorfWrap(t *testing.T) {
+	err := fmt.Errorf("wrapped: %w", &cause{})
+	fmt.Println(err)                // wrapped: cause!
+	fmt.Println(errors.Unwrap(err)) // cause!
+}
+
+func TestErrorsIs(t *testing.T) {
+	err := fmt.Errorf("wrapped: %w", &cause{})
+	log.Println(errors.Is(err, &cause{})) // true
+}
+
+func TestErrorsAs(t *testing.T) {
+	err := fmt.Errorf("wrapped: %w", &cause{})
+	var causeError *cause
+	if ok := errors.As(err, &causeError); ok {
+		log.Println(causeError) // cause!  err chainからcauseErrorに合う型をさがしてセットする
+	}
+}
