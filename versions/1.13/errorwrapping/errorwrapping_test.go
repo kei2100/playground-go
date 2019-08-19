05be4cd659dfe5876b331830ee5395bfdd2a98b0
kei2100 <kei.arima.2100@gmail.com>
2019-08-19 09:37:05 +0900

    add

--- versions/1.13/errorwrapping/errorwrapping_test.go
+++ versions/1.13/errorwrapping/errorwrapping_test.go
@@ -3,7 +3,6 @@ package errorwrapping
 import (
 	"errors"
 	"fmt"
-	"log"
 	"testing"
 )
 
@@ -21,17 +20,37 @@ func TestFmtErrorfWrap(t *testing.T) {
 
 func TestErrorsIs(t *testing.T) {
 	err := fmt.Errorf("wrapped: %w", &cause{})
-	log.Println(errors.Is(err, &cause{})) // true
+	fmt.Println(errors.Is(err, &cause{})) // true
 }
 
 func TestErrorsAs(t *testing.T) {
 	err := fmt.Errorf("wrapped: %w", &cause{})
 	var causeError *cause
 	if ok := errors.As(err, &causeError); ok {
-		log.Println(causeError) // cause!  err chainからcauseErrorに合う型をさがしてセットする
+		fmt.Println(causeError) // cause!  err chainからcauseErrorに合う型をさがしてセットする
 	}
 }
 
-// TODO
-type CustomError struct {
+type customError struct {
+	w error
+}
+
+func (*customError) Error() string {
+	return "customError"
+}
+
+func (e *customError) Unwrap() error {
+	return e.w
+}
+
+func TestCustomError(t *testing.T) {
+	err := &customError{w: &cause{}}
+	fmt.Println(err) // customError
+
+	fmt.Println(errors.Unwrap(err))       // cause!
+	fmt.Println(errors.Is(err, &cause{})) // true
+	var causeError *cause
+	if ok := errors.As(err, &causeError); ok {
+		fmt.Println(causeError) // cause!  err chainからcauseErrorに合う型をさがしてセットする
+	}
 }
