9dc99fd33c885b2719305ebb5a1058ba65631be2
kei2100 <kei.arima.2100@gmail.com>
2020-12-05 17:14:19 +0900

    add

---
+++ stdpkg/http/create_chunked_request_test.go
@@ -0,0 +1,6 @@
+package http
+
+// 		// create chunked request
+//		req, _ := http.NewRequest("POST", origin502, bytes.NewBufferString("foo"))
+//		req.ContentLength = -1
+//		req.Write(bytes.NewBufferString("bar"))
